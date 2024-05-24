8fb1bda92a5807b964e8ad6ab2e31dea22730c3d
satyam sharma <ss8454015344@gmail.com>
2024-05-24 23:32:45 +0530

    chore: Remove generateOTP utility function and passport strategies for Google and Facebook authentication

--- utils/generateOTP.js
+++
@@ -1,9 +0,0 @@
-//generate OTP
-
-const generateOTP = async (req, res, next) => {
-    const otp = Math.floor(1000 + Math.random() * 6000);//  
-
-    return otp;
-}
-
-module.exports = generateOTP;
